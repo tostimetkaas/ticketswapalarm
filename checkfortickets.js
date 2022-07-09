bd9c6758adf639184c00aaf0a673e2cfe1523d70
Kimberly <31919758+tostimetkaas@users.noreply.github.com>
2022-07-08 18:31:41 -0700

    js script to run after reload

---
+++ checkfortickets.js
@@ -0,0 +1,5 @@
+// span met "er zijn geen tickets beschikbaar"
+if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length < 1) {
+  const audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+  audio.play();
+}
