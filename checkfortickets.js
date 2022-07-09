100fe9ecc8470ded59ba41a4a21ff12affbfc610
Kimberly <31919758+tostimetkaas@users.noreply.github.com>
2022-07-08 18:48:14 -0700

    CORS workaround?

--- checkfortickets.js
+++ checkfortickets.js
@@ -1,5 +1,12 @@
 // span met "er zijn geen tickets beschikbaar"
 if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length < 1) {
-  const audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+  let audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+  //audio.crossOrigin = "anonymous";
   audio.play();
 }
+
+// testfunc: play sound if the
+// if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length == 1) {
+//   let audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+//   audio.play();
+// }
