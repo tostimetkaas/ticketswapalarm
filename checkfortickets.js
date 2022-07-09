e82c1c75ff49980ec760fc31217fb274defbaacb
Kimberly <31919758+tostimetkaas@users.noreply.github.com>
2022-07-08 20:37:35 -0700

    Update checkfortickets.js
    refactor into audio play function + 3 ways to check if tickets are available, third option is prefered (no manual input from inspector needed)

--- checkfortickets.js
+++ checkfortickets.js
@@ -1,12 +1,41 @@
-// span met "er zijn geen tickets beschikbaar"
-if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length < 1) {
+/* Audio play function
+ */
+const playSound = () => {
   let audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
   //audio.crossOrigin = "anonymous";
   audio.play();
 }
 
-// testfunc: play sound if the
-// if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length == 1) {
-//   let audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
-//   audio.play();
-// }
+
+/* Optie 1:
+ * span met "er zijn geen tickets beschikbaar", check of dit het enige element is in deze div
+ * NOTE: className moet handmatig met f12 gepasted worden :c
+ */
+if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length < 1) playSound();
+
+// testfunc: play sound if the div contains exactly one element (test with sold out event)
+// if(document.getElementsByClassName("css-y9qk84 e6fq7ah0").length == 1) playSound();
+
+
+/* Optie 2:
+ * check of "Er worden op dit moment geen tickets aangeboden." op de pagina staat. Zo niet, dan zijn er tickets
+ */
+// const noTicketsText = "Er worden op dit moment geen tickets aangeboden.";
+// if (document.body.textContent.includes(noTicketsText)) playSound();
+// ==> ISSUE: vindt ook tekst als dit niet op de pagina zichtbaar is
+
+
+/* Optie 3:
+ * Zoek naar Beschikbare tickets heading en check of de lijst minstens 1 link bevat met de tekst "ticket",
+ */
+const searchHeading = "Beschikbare tickets";
+const searchListing = "ticket";
+const headings = Array.from(document.getElementsByTagName("h3"));
+headings.forEach((heading, i) => {
+  if (heading.innerHTML == searchHeading) { // Ticketlisting gevonden, check of next element link naar ticket bevat
+    const ticketListing = heading.nextSibling;
+    if (ticketListing)
+      if (ticketListing.tagName == "A" &&
+          ticketListing.innerText.includes(searchListing)) playSound();
+  }
+});
